385561b1b0739d2bbf51a5273c615aa3f10c3589
Prateek Singh <singhprateek089@gmail.com>
2020-07-23 11:12:46 +0530

    angular-material-tutorial

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,43 @@
+import {Component} from '@angular/core';
+import {DataService} from '../data/data.service';
+import {Post} from '../Post';
+import {DataSource} from '@angular/cdk/table';
+import {Observable} from 'rxjs/Observable';
+
+import {AuthService} from '../auth.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent {
+  constructor(private dataService: DataService, public auth: AuthService) {
+  }
+
+  displayedColumns = ['date_posted', 'title', 'category', 'delete'];
+  dataSource = new PostDataSource(this.dataService);
+  deletePost(id) {
+    if (this.auth.isAuthenticated()){
+      this.dataService.deletePost(id);
+      this.dataSource = new PostDataSource(this.dataService);
+    }else {
+      alert('Login in Before');
+    }
+  }
+}
+
+
+
+export class PostDataSource extends DataSource<any> {
+  constructor(private dataService: DataService) {
+    super();
+  }
+
+  connect(): Observable<Post[]> {
+    return this.dataService.getData();
+  }
+
+  disconnect() {
+  }
+}
